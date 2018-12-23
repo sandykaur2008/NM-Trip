11ffc85623e69ab2719f7e7fb1a4c4c6e4e3c100
Satinder Kaur <sandykaur2008@gmail.com>
2018-12-23 15:09:03 -0500

    updated tests

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,5 +1,5 @@
 import unittest 
-from app import app, mail
+from app import app
 
 
 class Routes(unittest.TestCase):
@@ -7,25 +7,15 @@ class Routes(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         self.client = app.test_client()
-        mail.init_app(app)
 
     def tearDown(self):
         pass
     
     def test_200(self):
         response1 = self.client.get('/')
-        response2 = self.client.get('/about')
-        response3 = self.client.get('/carlsbad')
+        response2 = self.client.get('/carlsbad')
         self.assertEqual(response1.status_code, 200)
         self.assertEqual(response2.status_code, 200)
-        self.assertEqual(response3.status_code, 200)
-    
-    def test_302(self):
-        response = self.client.post('/contact', data={
-            'text': 'some text',
-            'email': 'ex@example.com',
-            'name': 'some name'}, follow_redirects=False)
-        self.assertEqual(response.status_code, 302)
 
     def test_404(self):
         response = self.client.get('/doesntexist')
